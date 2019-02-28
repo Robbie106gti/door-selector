4edd3a423649cd86fa7d3076f0252b6696722812
Robbie106gti <rleeuwerink@gmail.com>
2019-02-28 15:21:06 -0800

    testing out some ideas

--- src/components/ui/Body.js
+++ src/components/ui/Body.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Body extends Component {
+  onBookmark() {
+    console.log('Clicked');
+  }
   render() {
+    const { testTitle, array } = this.props;
     return (
       <div>
-        <h4>Body component</h4>
+        <h4>{testTitle}</h4>
         <ul>
-          <li>test 1</li>
-          <li>test 2</li>
+          {array.map(a => (
+            <li key={a}>
+              {a}{' '}
+              <i className="material-icons" onClick={this.onBookmark}>
+                bookmark
+              </i>
+            </li>
+          ))}
         </ul>
       </div>
     );
   }
 }
+
+Body.defaultProps = {
+  testTitle: 'Body component',
+  array: ['Test 1', 'Test 2']
+};
+
+Body.propTypes = {
+  testTitle: PropTypes.string.isRequired,
+  array: PropTypes.array.isRequired
+};
