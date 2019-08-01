3b2dfb640a815896e5e32bd7cc312315d6e80f12
robbie106gti <rleeuwerink@gmail.com>
2019-08-01 08:44:03 -0700

    carsoul changed to main image door and stain

--- src/components/pages/door/DoorStain.js
+++ src/components/pages/door/DoorStain.js
@@ -8,13 +8,14 @@ const DoorStain = (params) => {
   params = params.params;
   // useStoreActions(store =>  store.clickedMainDoor(params));
   const state = useStoreState(store => store.getDoorWoodStains(params));
-  // console.log(state)
+   console.log(state)
   return state ? (
     <div className="twoColumn">
       <div className="card-panel">
         <Link to={'./doors'} className="right"><span>{'<= Back'}</span></Link>
         <h2>{state.door.title}</h2>
-        <Carousel images={state.door.images.all} key="carousel" />
+        <div className="card-content"><img src={state.door.images.mainImage} alt={state.door.title} className="responsive-img" /></div>
+        <h2>{state.material.title}</h2>
         <div className="card-content"><img src={state.material.image} alt={state.material.title} className="responsive-img" /></div>
       </div>
       <div className="card-panel">
